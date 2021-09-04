fc8169959f270a02fdc1f29b01fe845f737c2632
joaolobao380 <victorlobao380@gmail.com>
2021-09-04 12:14:58 -0300

    fix env

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -10,9 +10,10 @@ module.exports = ({ env }) => ({
         database: env('DATABASE_NAME'),
         username: env('DATABASE_USERNAME'),
         password: env('DATABASE_PASSWORD'),
-        ssl: {
+        ssl: env.bool("DATABASE_SSL", false) ? {
+          require: true,
           rejectUnauthorized: false,
-        },
+        } : false,
       },
       options: {}
     },
